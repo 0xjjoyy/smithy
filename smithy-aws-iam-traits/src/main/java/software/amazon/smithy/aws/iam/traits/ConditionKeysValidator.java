446d7e60a6fa1129af758906b76e8ffeb56e8e77
Josh Joy <jjjoy@amazon.com>
2023-07-26 01:58:37 -0400

    pass trait to the results

--- smithy-aws-iam-traits/src/main/java/software/amazon/smithy/aws/iam/traits/ConditionKeysValidator.java
+++ smithy-aws-iam-traits/src/main/java/software/amazon/smithy/aws/iam/traits/ConditionKeysValidator.java
@@ -101,7 +101,7 @@ public final class ConditionKeysValidator extends AbstractValidator {
                                             conditionKey, ValidationUtils.tickedList(knownKeys))));
                                 }
                                 if (serviceResolvedKeys.contains(conditionKey)) {
-                                    results.add(error(memberShape, String.format(
+                                    results.add(error(memberShape, trait, String.format(
                                             "This operation `%s` scoped within the `%s` service with member `%s` refers"
                                                     + " to a condition key `%s` that is also resolved by service.",
                                             operation.getId(), service.getId(), memberShape.getId(),
